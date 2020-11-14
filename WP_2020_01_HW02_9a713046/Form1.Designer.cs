b22cdee549da61cb9e2d53544770821cfde110e5
SanboWang <loveforeverxiu@gmail.com>
2020-11-14 19:06:50 +0800

    加入專案檔案。

---
+++ WP_2020_01_HW02_9a713046/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WP_2020_01_HW02_9a713046
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picResult = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.button1.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(248, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "抽牌";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.Pink;
+            this.label1.Location = new System.Drawing.Point(242, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "剩餘排數為:52";
+            // 
+            // picResult
+            // 
+            this.picResult.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picResult.Cursor = System.Windows.Forms.Cursors.No;
+            this.picResult.Image = global::WP_2020_01_HW02_9a713046.Properties.Resources.g52;
+            this.picResult.Location = new System.Drawing.Point(578, 151);
+            this.picResult.Name = "picResult";
+            this.picResult.Size = new System.Drawing.Size(142, 211);
+            this.picResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picResult.TabIndex = 2;
+            this.picResult.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WP_2020_01_HW02_9a713046.Properties.Resources._001;
+            this.ClientSize = new System.Drawing.Size(1084, 662);
+            this.Controls.Add(this.picResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "抽牌程式";
+            this.Load += new System.EventHandler(this.frmGame);
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picResult;
+    }
+}
+
---
+++ WP_2020_01_HW02_9a713046/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WP_2020_01_HW02_9a713046.Properties;
+
+namespace WP_2020_01_HW02_9a713046
+{
+    public partial class Form1 : Form
+    {
+        List<Image> list = new List<Image>();   //讀檔
+        int brank = 52;                         //宣告一個數
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region 加入圖片
+
+        
+        private void frmGame(object sender, EventArgs e)
+        {
+            list.Add(Resources.g0);
+            list.Add(Resources.g1);
+            list.Add(Resources.g2);
+            list.Add(Resources.g3);
+            list.Add(Resources.g4);
+            list.Add(Resources.g5);
+            list.Add(Resources.g6); 
+            list.Add(Resources.g7);
+            list.Add(Resources.g8);
+            list.Add(Resources.g9);
+            list.Add(Resources.g10);
+            list.Add(Resources.g11);
+            list.Add(Resources.g12);
+            list.Add(Resources.g13);
+            list.Add(Resources.g14);
+            list.Add(Resources.g15);
+            list.Add(Resources.g16);
+            list.Add(Resources.g17);
+            list.Add(Resources.g18);
+            list.Add(Resources.g19);
+            list.Add(Resources.g20);
+            list.Add(Resources.g21);
+            list.Add(Resources.g22);
+            list.Add(Resources.g23);
+            list.Add(Resources.g24);
+            list.Add(Resources.g25);
+            list.Add(Resources.g26);
+            list.Add(Resources.g27);
+            list.Add(Resources.g28);
+            list.Add(Resources.g29);
+            list.Add(Resources.g30);
+            list.Add(Resources.g31);
+            list.Add(Resources.g32);
+            list.Add(Resources.g33);
+            list.Add(Resources.g34);
+            list.Add(Resources.g35);
+            list.Add(Resources.g36);
+            list.Add(Resources.g37);
+            list.Add(Resources.g38);
+            list.Add(Resources.g39);
+            list.Add(Resources.g40);
+            list.Add(Resources.g41);
+            list.Add(Resources.g42);
+            list.Add(Resources.g43);
+            list.Add(Resources.g44);
+            list.Add(Resources.g45);
+            list.Add(Resources.g46);
+            list.Add(Resources.g47);
+            list.Add(Resources.g48);
+            list.Add(Resources.g49);
+            list.Add(Resources.g50);
+            list.Add(Resources.g51);
+        }
+        #endregion
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            Random poker = new Random();                        //產生亂數
+            int image = poker.Next(52);                         //共52張圖
+            picResult.Image = list[image];                      //輸出圖片
+
+            brank--;                                            //遞減
+            label1.Text = ("剩餘牌數為:" + brank);              //遞減後輸出結果
+            if (brank <= 0)                                     //條件
+            {
+                button1.Enabled = false;                        //條件成立輸出結果
+            }
+
+        }
+    }
+}
